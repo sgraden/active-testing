d1e6957afec27fffd43dc452908be92d58a51290
STEVEN G. RADEN <sgraden@us.ibm.com>
2017-04-18 16:07:43 -0500

    nothing

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.use(express.static(__dirname + '/views'));
 
 
 
-var port = $PORT || 3000
+var port = process.env.PORT || 3000
 app.listen(port, function() {
     console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
