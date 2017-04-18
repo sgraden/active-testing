2e7b6f49485fca10ac589a1399d8ab26924dcd42
STEVEN G. RADEN <sgraden@us.ibm.com>
2017-04-18 14:46:56 -0500

    background

---
+++ index.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let app = express();
+let path = require('path');
+
+var public = path.join(__dirname, '/pub');
+app.use(express.static(public));
+
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(3000, () => {
+    console.log("App listening on 3000");
+})
